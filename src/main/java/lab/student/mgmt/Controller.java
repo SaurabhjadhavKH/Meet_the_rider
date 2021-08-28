7a0aba8250a2036ec97ef0d9b300d6a9bf89b4f8
SaurabhjadhavKH <saurabhjadhav9308@gmail.com>
2021-08-28 13:46:53 +0530

    Create the Test api for testing

---
+++ src/main/java/lab/student/mgmt/Controller.java
@@ -0,0 +1,13 @@
+package lab.student.mgmt;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class Controller {
+
+	@GetMapping("test")
+	public String test() {
+		return "Testing...";
+	}
+}
