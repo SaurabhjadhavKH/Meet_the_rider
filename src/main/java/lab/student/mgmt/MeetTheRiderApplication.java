26e5e361096c0f9f588d90822f40cf963f1c29b5
SaurabhjadhavKH <saurabhjadhav9308@gmail.com>
2021-08-28 13:38:51 +0530

    Initilized the Project

---
+++ src/main/java/lab/student/mgmt/MeetTheRiderApplication.java
@@ -0,0 +1,13 @@
+package lab.student.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetTheRiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetTheRiderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/student/mgmt/MeetTheRiderApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.student.mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetTheRiderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
